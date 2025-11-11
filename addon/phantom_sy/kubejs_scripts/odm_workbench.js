a845d4bf03cf48da08f562c43f75155f38b4ed9a
ThePhantomPig791 <ThePhantomPig791@gmail.com>
2025-11-10 23:42:08 -0500

    removed deprecated odm workbench in favor of the upcoming tailoring bench

--- addon/phantom_sy/kubejs_scripts/odm_workbench.js
+++
@@ -1,8 +0,0 @@
-StartupEvents.registry('block', event => {
-    event.create('phantom_sy:odm_workbench')
-        .soundType('wood')
-        .blockEntity(blockEntityInfo => {
-            blockEntityInfo.inventory(9, 1);
-            blockEntityInfo.enableSync();
-        })
-})
