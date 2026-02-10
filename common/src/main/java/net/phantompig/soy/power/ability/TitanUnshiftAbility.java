f9b46eec13c81b552ccb3f05764d1cea50cbbac1
ThePhantomPig791 <ThePhantomPig791@gmail.com>
2026-02-09 20:44:49 -0500

    tried to reset titan stuff on death, it kinda half works

--- common/src/main/java/net/phantompig/soy/power/ability/TitanUnshiftAbility.java
+++ common/src/main/java/net/phantompig/soy/power/ability/TitanUnshiftAbility.java
@@ -1,5 +1,6 @@
 package net.phantompig.soy.power.ability;
 
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Items;
 import net.phantompig.soy.player.SoyPlayerExtension;
@@ -9,8 +10,10 @@ import net.threetag.palladium.power.ability.AbilityInstance;
 import net.threetag.palladium.util.icon.ItemIcon;
 import net.threetag.palladium.util.property.BooleanProperty;
 import net.threetag.palladium.util.property.PalladiumProperty;
+import net.threetag.palladiumcore.event.EventResult;
+import net.threetag.palladiumcore.event.LivingEntityEvents;
 
-public class TitanUnshiftAbility extends Ability {
+public class TitanUnshiftAbility extends Ability implements LivingEntityEvents.Death {
     public static final PalladiumProperty<Boolean> SPAWN_SKELETON = new BooleanProperty("spawn_skeleton").configurable("If true, leaves behind a decaying titan body");
 
     public TitanUnshiftAbility() {
@@ -18,6 +21,8 @@ public class TitanUnshiftAbility extends Ability {
 
         this.withProperty(ICON, new ItemIcon(Items.ROTTEN_FLESH));
         this.withProperty(HIDDEN_IN_BAR, false);
+
+        LivingEntityEvents.DEATH.register(this);
     }
 
     @Override
@@ -32,5 +37,14 @@ public class TitanUnshiftAbility extends Ability {
             titanInstance.resetScale();
             titanInstance.titan.unshift(entity);
         }
+    }
+
+    @Override
+    public EventResult livingEntityDeath(LivingEntity entity, DamageSource damageSource) {
+        if (!(entity instanceof SoyPlayerExtension soy)) return EventResult.pass();
+        var titanInstance = soy.getTitanInstance();
+        if (titanInstance.titan == null) return EventResult.pass();
+        titanInstance.forceUnshift = true;
+        return EventResult.cancel();
     }
 }
