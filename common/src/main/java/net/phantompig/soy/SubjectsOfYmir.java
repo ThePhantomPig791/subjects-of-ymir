b983610ae463d999085a190b37fb6f254a094be9
thephantompig791 <ThePhantomPig791@gmail.com>
2026-03-19 20:33:20 -0400

    oops removed extraneous event subscription

--- common/src/main/java/net/phantompig/soy/SubjectsOfYmir.java
+++ common/src/main/java/net/phantompig/soy/SubjectsOfYmir.java
@@ -90,11 +90,6 @@ public class SubjectsOfYmir {
                 newExt.setTitanInstance(TitanInstance.fromTag(newPlayer, oldExt.getTitanInstance().toTag()));
             }
         }));
-
-        ChatEvents.SERVER_SUBMITTED.register((player, raw, message) -> {
-            message.copy().withStyle(style -> style.withObfuscated(true));
-            return EventResult.pass();
-        });
     }
 
     public static ResourceLocation rsrc(String path) {
