70eb41577dfb666e6e2b47aca15468cd54963c98
ThePhantomPig791 <ThePhantomPig791@gmail.com>
2026-05-02 13:44:33 -0400

    JEI info for iron bamboo + leaf

--- common/src/main/java/net/phantompig/soy/compat/jei/SoyJeiPlugin.java
+++ common/src/main/java/net/phantompig/soy/compat/jei/SoyJeiPlugin.java
@@ -2,12 +2,14 @@ package net.phantompig.soy.compat.jei;
 
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.registration.IRecipeCatalystRegistration;
 import mezz.jei.api.registration.IRecipeCategoryRegistration;
 import mezz.jei.api.registration.IRecipeRegistration;
 import mezz.jei.api.registration.IRecipeTransferRegistration;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.crafting.RecipeManager;
 import net.phantompig.soy.SubjectsOfYmir;
@@ -16,6 +18,7 @@ import net.phantompig.soy.menu.CompressionMenu;
 import net.phantompig.soy.menu.SoyMenus;
 import net.phantompig.soy.recipe.SoyRecipeTypes;
 
+import java.util.List;
 import java.util.Objects;
 
 @JeiPlugin
@@ -36,6 +39,8 @@ public class SoyJeiPlugin implements IModPlugin {
         RecipeManager recipeManager = level.getRecipeManager();
 
         registration.addRecipes(CompressionCategory.RECIPE_TYPE, recipeManager.getAllRecipesFor(SoyRecipeTypes.COMPRESSION.get()));
+
+        registration.addItemStackInfo(List.of(SoyItems.IRON_BAMBOO.get().getDefaultInstance(), SoyItems.IRON_BAMBOO_LEAF.get().getDefaultInstance()), Component.translatable("info.item.iron_bamboo"), Component.translatable("info.item.iron_bamboo_leaf"));
     }
 
     @Override
